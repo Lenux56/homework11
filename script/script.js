08b1c21c24aa463eea16fdc34b1e9221aafa48a8
Lenux56 <popelyshelena@gmail.com>
2022-04-15 19:52:14 +0300

    localstorage

--- script/script.js
+++ script/script.js
@@ -1,70 +1,100 @@
-const blockElem = document.querySelector('#blocks');
-const cardList = [];
+
+const readCards = () => JSON.parse(localStorage.getItem('cards')) || [];
+const writeCards = data => localStorage.setItem('cards', JSON.stringify([...readCards(), data]));
+const removeCard = data => {
+	const newLst = readCards().filter(elem => JSON.stringify(elem) !== JSON.stringify(data));
+	localStorage.setItem('cards', JSON.stringify(newLst));
+}
+
+function changeCard (data, state){
+	const cardIndex = readCards().findIndex(element =>	element.id === data.id);
+	let newList = readCards();
+	newList[cardIndex].state = state;
+	localStorage.setItem('cards', JSON.stringify(newList));
+}
 
 const formElem = document.forms[0];
 const seaechRoot = document.querySelector('#search');
 const searchElem = seaechRoot.querySelector('input');
 
-function translate(event) {
-	console.log(event)
-}
+//from stackoverflow
+const generateUUID = () => {
+  let
+    d = new Date().getTime(),
+    d2 = (performance && performance.now && (performance.now() * 1000)) || 0;
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
+    let r = Math.random() * 16;
+    if (d > 0) {
+      r = (d + r) % 16 | 0;
+      d = Math.floor(d / 16);
+    } else {
+      r = (d2 + r) % 16 | 0;
+      d2 = Math.floor(d2 / 16);
+    }
+    return (c == 'x' ? r : (r & 0x7 | 0x8)).toString(16);
+  });
+};
 
 function render(list){
+	const blockElem = document.querySelector('#blocks');
 	blockElem.innerText = '';
-	list.forEach((element, index, object) => {
-		const cardElem = document.createElement('div');
+	blockElem.append(
+		...list.map(card => {
+			const {cardName, cardTranslate, cardColor, state} = card;
+			const cardElem = document.createElement('div');
+			cardElem.classList.add('card');
+			cardElem.style.backgroundColor = cardColor; 
+			cardElem.innerText = (state === 1) ? cardTranslate : cardName; 
 
-		cardElem.classList.add('card');
-		cardElem.style.backgroundColor = element.cardColor; 
-		cardElem.innerText = (element.state === 1) ? element.cardTranslate : element.cardName; 
+	  		const crossElem = document.createElement('div');
+	  		crossElem.classList.add('cross');
 
-  		const crossElem = document.createElement('div');
-  		crossElem.classList.add('cross');
+	  		cardElem.appendChild(crossElem);
+	  		
+	  		crossElem.addEventListener('click', ()=>{
+	  			removeCard(card);
+	  			render(readCards());
+	  		});
 
-  		cardElem.appendChild(crossElem);
-  		blockElem.appendChild(cardElem);
-
-  		crossElem.addEventListener('click', ()=>{
-  			object.splice(index, 1);
-  			render(cardList);
-  		});
-
-  		cardElem.addEventListener('dblclick', event => {
-  			if (element.state === 1){
-  				element.state = 0;	
-  			}else{
-  				element.state = 1;
-  			}
-  			render(cardList);
-  		});
-  	});
-  	console.log(cardList);	
+	  		cardElem.addEventListener('dblclick', event => {
+	  			if (card.state === 1){
+	  				//console.log(card);
+	  				//console.log(readCards());
+	  				//card.state = 0;	
+	  				changeCard(card, 0);
+	  				//console.log(readCards());
+	  				render(readCards());
+	  			}else{
+	  				//card.state = 1;
+	  				//console.log(card);
+	  				//console.log(readCards());
+	  				changeCard(card, 1);
+	  				//console.log(readCards());
+	  				render(readCards());
+	  			}
+	  			render(readCards());
+	  		});
+	  		return cardElem
+	  	}));	
 }
 
 formElem.addEventListener('submit', event=>{
 	event.preventDefault();
 
-	const cardName = event.target.cardName;
-	const cardTranslate = event.target.cardTranslate;
-	const cardColor = event.target.cardColor;
+	const cardName = event.target.cardName.value;
+	const cardTranslate = event.target.cardTranslate.value;
+	const cardColor = event.target.cardColor.value;
 
-	if (cardName.value === '' || cardTranslate.value === '' || cardColor.value === '' ){
+	if (cardName === '' || cardTranslate === '' || cardColor === '' ){
 		alert('Необходимо заполнить все поля');
 	}else{
-		const objELem = {
-			[cardName.name]: cardName.value,
-			[cardTranslate.name]: cardTranslate.value,
-			[cardColor.name]: cardColor.value,
-			'state' : 0
-		};
-		cardList.push(objELem);
 
+		writeCards({'id': generateUUID(), cardName, cardTranslate, cardColor, 'state' : 0});
 	}	
-	
-	cardName.value = '';
-	cardTranslate.value = '';
-	cardColor.value = '';
-	render(cardList);
+	event.target.cardName.value = '';
+	event.target.cardTranslate.value = '';
+	event.target.cardColor.value = '';
+	render(readCards());
 	searchElem.value = '';
 })
 
@@ -73,12 +103,11 @@ searchElem.addEventListener('input', event=>{
 	
 	const searchText = event.target;
 	if (searchText.value === ''){
-		render(cardList);
+		render(readCards());
 	}else{
-
-	render(cardList.filter(elem => 
-		(elem.state === 0) ? elem.cardName.startsWith(searchText.value) : elem.cardTranslate.startsWith(searchText.value)));
+		render(readCards().filter(elem => 
+			(elem.state === 0) ? elem.cardName.startsWith(searchText.value) : elem.cardTranslate.startsWith(searchText.value)));
 	}
 })
 
-render(cardList);
+render(readCards());
